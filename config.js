2525988faa993db7bdf5cf35ebf346e61ef592d0
Telmaftorres <telma.ferreiratorres@gmail.com>
2026-06-01 09:59:29 +0200

    feat: bouton 📊 accès direct au Google Sheet

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  // Remplace par ton URL Apps Script après déploiement
   API_URL: 'https://script.google.com/macros/s/AKfycbxGMdMvzLvbx8uhJwUQkB7OXXh21JUNnyfAmlQ8PCP-drpxHGCeiUqiY__pI3Onxoh1/exec',
+  SHEET_URL: 'https://docs.google.com/spreadsheets/d/1mJ5MBZ0zl9D137Cc_reiHMkVG6m9mr6uVZyfVdFzZGc/edit',
   APP_NAME: 'Kontfeel Stock',
 };
